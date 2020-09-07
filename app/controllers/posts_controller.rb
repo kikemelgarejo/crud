e2ca0766e7ef1353023bdc425c369627a6d661b3
kikemelga <k.melgarejo123@gmail.com>
2020-09-07 13:36:56 -0300

    refactoring views, controllers and routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+
+    before_action :find_post, only: [:show, :edit, :update, :destroy]
+
     def index
         @posts = Post.all
     end
@@ -14,21 +17,17 @@ class PostsController < ApplicationController
     end
 
     def show
-        @post = Post.find(params[:id])
     end
 
     def edit
-        @post = Post.find(params[:id])
     end
 
     def update
-        @post = Post.find(params[:id])
         @post.update(post_params)
         redirect_to post_path
     end
 
     def destroy
-        @post = Post.find(params[:id])
         @post.destroy
         redirect_to posts_path
     end
@@ -38,5 +37,9 @@ class PostsController < ApplicationController
 
     def post_params
         params.require(:post).permit(:title, :content)
+    end
+
+    def find_post
+        @post = Post.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'posts', to: 'posts#index' #to especifíca como exactamente queremos que se procese la acción
-  get 'posts/new'
-  post 'posts', to: 'posts#create'
-  get 'posts/:id', to: 'posts#show', as: 'post'
-  get 'posts/:id/edit', to: 'posts#edit'
-  patch 'posts/:id', to: 'posts#update'
-  delete 'posts/:id', to: 'posts#destroy'
-
+resources :posts
 end
