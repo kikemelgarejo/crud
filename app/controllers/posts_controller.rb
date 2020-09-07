9cbf9149303ea30bc597fe4e805f9016f1dc3f07
kikemelga <k.melgarejo123@gmail.com>
2020-09-07 12:52:48 -0300

    manual create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,15 @@ class PostsController < ApplicationController
     def new
         @post = Post.new
     end
+
+    def create
+        @post = Post.new(post_params)
+        @post.save
+        redirect_to posts_path
+    end
+
+    private
+    def post_params
+        params.require(:post).permit(:title, :content)
+    end
 end
